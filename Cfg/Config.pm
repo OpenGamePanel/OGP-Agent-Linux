9ce6ed21f61c4178f0ed6de55adf7ff95a798de3
Andre <andregomesest@gmail.com>
2014-02-20 10:09:04 -0300

    First Commit Linux Agent

---
+++ Cfg/Config.pm
@@ -0,0 +1,8 @@
+%Cfg::Config = (
+    logfile => 'ogp_agent.log',
+    listen_port  => '12679',
+    listen_ip => '0.0.0.0',
+    version => 'v1.0',
+    key => 'test1',
+    steam_license => 'Accept',
+    );
---
+++ README.md
@@ -0,0 +1,5 @@
+Open Game Panel Linux Agent
+===============
+This is the linux agent of the Open Game Panel project
+
+Check the install instructions at www.opengamepanel.org
