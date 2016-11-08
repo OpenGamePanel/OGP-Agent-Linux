cf2da5d27c386ef6ea4439db4561d20e23c5c375
DieFeM <danifmdelloret@hotmail.com>
2016-11-08 20:18:36 +0100

    Cleaning up.

--- Cfg/Config.pm
+++
@@ -1,8 +0,0 @@
-%Cfg::Config = (
-    logfile => 'ogp_agent.log',
-    listen_port  => '12679',
-    listen_ip => '0.0.0.0',
-    version => 'v1.0',
-    key => 'test1',
-    steam_license => 'Accept',
-    );
