b96010be72a3391cb5c75bda5fa81fc829c6af3d
®omano <zebilaweed@free.fr>
2017-05-24 23:28:35 +0200

    Noticed that first iteration of top also isn't acurrate...
    ...so I propose this change to have results from second iteration of top. If you have better solution please modify this PR.

--- ogp_agent.pl
+++ ogp_agent.pl
@@ -3833,7 +3833,7 @@ sub shell_action
 	elsif($action eq 'get_tasklist')
 	{
 		my %taskList;
-		$taskList{'task'} = encode_base64(`top -b -c -o +%CPU -w512 -n1|grep "COMMAND" -A 30`);
+		$taskList{'task'} = encode_base64(`top -b -c -o +%CPU -w512 -n2|grep "COMMAND" -A 30|tail -36`);
 		return {%taskList};
 	}
 	elsif($action eq 'get_timestamp')
