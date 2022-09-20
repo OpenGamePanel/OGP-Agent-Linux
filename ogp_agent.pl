71b41e87d9b14eeddce077b062824bd455e2e5f9
own3mall <own3mall@gmail.com>
2022-09-20 15:41:03 -0600

    Security Changes

--- ogp_agent.pl
+++ ogp_agent.pl
@@ -87,6 +87,7 @@ use constant SCREENRC_TMP_FILE =>
   Path::Class::File->new(AGENT_RUN_DIR, 'ogp_screenrc.tmp');
 use constant SCREEN_TYPE_HOME   => "HOME";
 use constant SCREEN_TYPE_UPDATE => "UPDATE";
+use constant SERVER_RUNNER_USER => "ogp_server_runner";
 use constant FD_DIR => Path::Class::Dir->new(AGENT_RUN_DIR, 'FastDownload');
 use constant FD_ALIASES_DIR => Path::Class::Dir->new(FD_DIR, 'aliases');
 use constant FD_PID_FILE => Path::Class::File->new(FD_DIR, 'fd.pid');
@@ -486,7 +487,20 @@ sub get_home_pids
 	my ($home_id) = @_;
 	my $screen_id = create_screen_id(SCREEN_TYPE_HOME, $home_id);
 	my ($pid, @pids);
-	($pid) = split(/\./, `screen -ls | grep -E -o "[0-9]+\.$screen_id"`, 2);
+	my $as_user = SERVER_RUNNER_USER;
+	my $ret = sudo_exec_without_decrypt('screen -ls | grep -E -o "[0-9]+\.'.$screen_id.'"', $as_user);
+	my ($retval, $enc_out) = split(/;/, $ret, 2);
+	if($retval != 1)
+	{
+		logger "Unable to get pids, probably a bad sudo password or not in sudoers list.";
+		return ();
+	}
+	
+	$enc_out =~ s/\\n//g;
+	
+	my $out = decode_base64($enc_out);
+	
+	($pid) = split(/\./, $out, 2);
 	if(defined $pid)
 	{
 		chomp($pid);
@@ -736,9 +750,26 @@ sub is_screen_running_without_decrypt
 	my ($screen_type, $home_id) = @_;
 
 	my $screen_id = create_screen_id($screen_type, $home_id);
-
-	my $is_running = `screen -list | grep $screen_id`;
-
+	
+	my $as_user = SERVER_RUNNER_USER;
+		
+	my $ret = sudo_exec_without_decrypt('screen -list | grep '.$screen_id, $as_user);
+		
+	my ($retval, $enc_out) = split(/;/, $ret, 2);
+	
+	if($retval != 1)
+	{
+		return 0;
+	}
+	
+	my $is_running = " ";
+	
+	if( defined($enc_out) )
+	{
+		$enc_out =~ s/\\n//g;
+		$is_running = decode_base64($enc_out);
+	}
+		
 	if ($is_running =~ /^\s*$/)
 	{
 		return 0;
@@ -782,19 +813,19 @@ sub universal_start_without_decrypt
 		return -14;
 	}
 	
+	secure_path_without_decrypt('chattr-i', $server_exe);
+	
 	if (!-e $home_path)
 	{
 		logger "Can't find server's install path [ $home_path ].";
 		return -10;
 	}
 	
-	my $uid = `id -u`;
-	chomp $uid;
-	my $gid = `id -g`;
-	chomp $gid;
-	my $path = $home_path;
-	$path =~ s/('+)/'\"$1\"'/g;
-	sudo_exec_without_decrypt('chown -Rf '.$uid.':'.$gid.' \''.$path.'\'');
+	my $owner = SERVER_RUNNER_USER;
+	my $group = SERVER_RUNNER_USER;
+	chomp $group;
+	
+	set_path_ownership($owner, $group, $home_path);
 	
 	# Some game require that we are in the directory where the binary is.
 	my $game_binary_dir = Path::Class::Dir->new($home_path, $run_dir);
@@ -804,11 +835,9 @@ sub universal_start_without_decrypt
 		return -12;
 	}
 	
-	secure_path_without_decrypt('chattr-i', $server_exe);
-	
 	if (!-x $server_exe)
 	{
-		if (!chmod 0755, $server_exe)
+		if (!chmod 0775, $server_exe)
 		{
 			logger "The $server_exe file is not executable.";
 			return -13;
@@ -941,9 +970,9 @@ sub universal_start_without_decrypt
 	  "Startup command [ $cli_bin ] will be executed in dir $game_binary_dir.";
 	
 	# Run before start script
-	$run_before_start = run_before_start_commands($home_id, $home_path, $preStart);
+	$run_before_start = run_before_start_commands($home_id, $home_path, $preStart, $owner);
 	
-	system($cli_bin);
+	sudo_exec_without_decrypt($cli_bin, $owner);
 	
 	sleep(1);
 	
@@ -1231,6 +1260,9 @@ sub stop_server_without_decrypt
 		logger("Invalid IP:Port given $server_ip:$server_port.");
 		return 1;
 	}
+	
+	my $screen_id = create_screen_id(SCREEN_TYPE_HOME, $home_id);
+	my $as_user = SERVER_RUNNER_USER;
 
 	if ($control_password !~ /^\s*$/ and $control_protocol ne "")
 	{
@@ -1362,21 +1394,23 @@ sub stop_server_without_decrypt
 		@server_pids = get_home_pids($home_id);
 		
 		my $cnt;
+		my $out;
 		foreach my $pid (@server_pids)
 		{
 			chomp($pid);
-			$cnt = kill 15, $pid;
-			
-			if ($cnt != 1)
+			$cnt = sudo_exec_without_decrypt("kill 15 $pid", $as_user);
+			($cnt, $out) = split(/;/, $cnt, 2);
+			if ($cnt == -1)
 			{
-				$cnt = kill 9, $pid;
-				if ($cnt == 1)
+				$cnt = sudo_exec_without_decrypt("kill 9 $pid", $as_user);
+				($cnt, $out) = split(/;/, $cnt, 2);
+				if ($cnt == -1)
 				{
-					logger "Stopped process with pid $pid successfully using kill 9.";
+					logger "Process $pid can not be stopped.";
 				}
 				else
 				{
-					logger "Process $pid can not be stopped.";
+					logger "Stopped process with pid $pid successfully using kill 9.";
 				}
 			}
 			else
@@ -1384,7 +1418,7 @@ sub stop_server_without_decrypt
 				logger "Stopped process with pid $pid successfully using kill 15.";
 			}
 		}
-		system('screen -wipe > /dev/null 2>&1');
+		sudo_exec_without_decrypt('screen -wipe > /dev/null 2>&1', $as_user);
 		return 0;
 	}
 	else
@@ -1393,21 +1427,23 @@ sub stop_server_without_decrypt
 		my @server_pids = get_home_pids($home_id);
 		
 		my $cnt;
+		my $out;
 		foreach my $pid (@server_pids)
 		{
 			chomp($pid);
-			$cnt = kill 15, $pid;
-			
-			if ($cnt != 1)
+			$cnt = sudo_exec_without_decrypt("kill 15 $pid", $as_user);
+			($cnt, $out) = split(/;/, $cnt, 2);
+			if ($cnt == -1)
 			{
-				$cnt = kill 9, $pid;
-				if ($cnt == 1)
+				$cnt = sudo_exec_without_decrypt("kill 9 $pid", $as_user);
+				($cnt, $out) = split(/;/, $cnt, 2);
+				if ($cnt == -1)
 				{
-					logger "Stopped process with pid $pid successfully using kill 9.";
+					logger "Process $pid can not be stopped.";
 				}
 				else
 				{
-					logger "Process $pid can not be stopped.";
+					logger "Stopped process with pid $pid successfully using kill 9.";
 				}
 			}
 			else
@@ -1415,7 +1451,7 @@ sub stop_server_without_decrypt
 				logger "Stopped process with pid $pid successfully using kill 15.";
 			}
 		}
-		system('screen -wipe > /dev/null 2>&1');
+		sudo_exec_without_decrypt('screen -wipe > /dev/null 2>&1', $as_user);
 		return 0;
 	}
 }
@@ -1936,7 +1972,7 @@ sub lock_additional_files_logic{
 sub run_before_start_commands
 {
 	#return "Bad Encryption Key" unless(decrypt_param(pop(@_)) eq "Encryption checking OK");
-	my ($server_id, $homedir, $beforestartcmd) = @_;
+	my ($server_id, $homedir, $beforestartcmd, $pathowner) = @_;
 	
 	if ($homedir ne "" && $server_id ne ""){
 		# Run any prestart scripts
@@ -1954,7 +1990,7 @@ sub run_before_start_commands
 			print FILE "rm -f $prestartcmdfile\n";
 			close FILE;
 			chmod 0755, $prestartcmdfile;
-			system("bash $prestartcmdfile");
+			sudo_exec_without_decrypt("bash $prestartcmdfile", $pathowner);
 		}		
 	}else{
 		return -2;
@@ -2007,13 +2043,8 @@ sub create_secure_script
 
 sub check_b4_chdir
 {
-	my ( $path ) = @_;
-	
-	my $uid = `id -u`;
-	chomp $uid;
-	my $gid = `id -g`;
-	chomp $gid;	
-		
+	my ($path, $owner) = @_;
+			
 	if (!-e $path)
 	{
 		logger "$path does not exist yet. Trying to create it...";
@@ -2029,16 +2060,12 @@ sub check_b4_chdir
 		{
 			return -1;
 		}
-		
-		# Set perms on it as well
-		sudo_exec_without_decrypt('chown -Rf '.$uid.':'.$gid.' \''.$path.'\'');
 	}
-	else
-	{	
-		# File or directory already exists
-		# Make sure it's owned by the agent
-		secure_path_without_decrypt('chattr-i', $path);
-	}
+	
+	my $group = SERVER_RUNNER_USER;
+	chomp $group;
+	
+	set_path_ownership($owner, $group, $path);
 	
 	if (!chdir $path)
 	{
@@ -2046,6 +2073,24 @@ sub check_b4_chdir
 		return -1;
 	}
 	
+	return 0;
+}
+
+sub set_path_ownership
+{
+	my ($owner, $group, $path) = @_;
+	
+	my $owner_uid = `id -u $owner`;
+	chomp $owner_uid;
+	my $group_uid = `id -u $group`;
+	chomp $group_uid;
+		
+	# Remove immutable flag recursivelly
+	secure_path_without_decrypt('chattr-i', $path);
+	# Set owner and perms on it recursivelly as well
+	sudo_exec_without_decrypt("chown -Rf $owner_uid:$group_uid '$path'");
+	sudo_exec_without_decrypt("chmod -Rf ug+rwx '$path'");
+		
 	return 0;
 }
 
@@ -2108,19 +2153,19 @@ sub start_rsync_install
 {
 	return "Bad Encryption Key" unless(decrypt_param(pop(@_)) eq "Encryption checking OK");
 	my ($home_id, $home_path, $url, $exec_folder_path, $exec_path, $precmd, $postcmd, $filesToLockUnlock) = decrypt_params(@_);
-
-	if ( check_b4_chdir($home_path) != 0)
+	
+	my $owner = SERVER_RUNNER_USER;
+	
+	if ( check_b4_chdir($home_path, $owner) != 0)
 	{
 		return 0;
 	}
-	
-	secure_path_without_decrypt('chattr-i', $home_path);
-	
+		
 	create_secure_script($home_path, $exec_folder_path, $exec_path);
-
+	
 	my $bash_scripts_path = MANUAL_TMP_DIR . "/home_id_" . $home_id;
 	
-	if ( check_b4_chdir($bash_scripts_path) != 0)
+	if ( check_b4_chdir($bash_scripts_path, $owner) != 0)
 	{
 		return 0;
 	}
@@ -2167,19 +2212,18 @@ sub master_server_update
 {
 	return "Bad Encryption Key" unless(decrypt_param(pop(@_)) eq "Encryption checking OK");
 	my ($home_id,$home_path,$ms_home_id,$ms_home_path,$exec_folder_path,$exec_path,$precmd,$postcmd) = decrypt_params(@_);
+	my $owner = SERVER_RUNNER_USER;
 	
-	if ( check_b4_chdir($home_path) != 0)
+	if ( check_b4_chdir($home_path, $owner) != 0)
 	{
 		return 0;
 	}
-	
-	secure_path_without_decrypt('chattr-i', $home_path);
-		
+			
 	create_secure_script($home_path, $exec_folder_path, $exec_path);
 			
 	my $bash_scripts_path = MANUAL_TMP_DIR . "/home_id_" . $home_id;
 	
-	if ( check_b4_chdir($bash_scripts_path) != 0)
+	if ( check_b4_chdir($bash_scripts_path, $owner) != 0)
 	{
 		return 0;
 	}
@@ -2239,19 +2283,18 @@ sub steam_cmd
 sub steam_cmd_without_decrypt
 {
 	my ($home_id, $home_path, $mod, $modname, $betaname, $betapwd, $user, $pass, $guard, $exec_folder_path, $exec_path, $precmd, $postcmd, $cfg_os, $filesToLockUnlock, $arch_bits) = @_;
+	my $owner = SERVER_RUNNER_USER;
 	
-	if ( check_b4_chdir($home_path) != 0)
+	if ( check_b4_chdir($home_path, $owner) != 0)
 	{
 		return 0;
 	}
-	
-	secure_path_without_decrypt('chattr-i', $home_path);
-	
+		
 	create_secure_script($home_path, $exec_folder_path, $exec_path);
 	
 	my $bash_scripts_path = MANUAL_TMP_DIR . "/home_id_" . $home_id;
 	
-	if ( check_b4_chdir($bash_scripts_path) != 0)
+	if ( check_b4_chdir($bash_scripts_path, $owner) != 0)
 	{
 		return 0;
 	}
@@ -2857,6 +2900,90 @@ sub restart_server_without_decrypt
 	}
 }
 
+sub find_user_by_screen_id
+{
+	my ($screen_id) = @_;
+	
+	my $screen_user = SERVER_RUNNER_USER;
+	
+	my $ret = sudo_exec_without_decrypt('find /var/run/screen -name "*'.$screen_id.'"');
+		
+	my ($retval, $enc_out) = split(/;/, $ret, 2);
+	
+	if($retval != 1)
+	{
+		return $screen_user;
+	}
+		
+	if( defined($enc_out) && $enc_out =~ /^(.+)\\n/ )
+	{
+		my @dec_out = ();
+		foreach my $line (split /\\n/, $enc_out) {
+			my $dec_line = decode_base64($line);
+			push @dec_out, $dec_line;
+		}
+		
+		my @path_parts = split /\//, $dec_out[0];
+		
+		if ($#path_parts == 5)
+		{
+			if($path_parts[5] =~ /^(\d+)\.$screen_id$/)
+			{
+				if ($path_parts[4] =~ /^S-/)
+				{
+					my $parseval = $path_parts[4];
+					$parseval =~ s/^S-//g;
+					my $uid = `id -u $parseval`;
+					if( $uid =~ /^(\d+)$/ )
+					{
+						$screen_user = $parseval;
+					}
+				}
+			}
+		}
+	}
+	
+	return $screen_user;
+}
+
+sub get_path_owner
+{
+	my ($path) = @_;
+	
+	my $path_owner = SERVER_RUNNER_USER;
+	
+	if(-d $path)
+	{
+		my $ret = sudo_exec_without_decrypt('stat -c "%U" "'.$path.'"');
+			
+		my ($retval, $enc_out) = split(/;/, $ret, 2);
+		
+		if($retval != 1)
+		{
+			return $path_owner;
+		}
+			
+		if( defined($enc_out) && $enc_out =~ /^(.+)\\n/ )
+		{
+			my @dec_out = ();
+			foreach my $line (split /\\n/, $enc_out) {
+				my $dec_line = decode_base64($line);
+				push @dec_out, $dec_line;
+			}
+
+			my $parseval = $dec_out[0];
+			my $uid = `id -u $parseval`;
+			
+			if( $uid =~ /^(\d+)$/ )
+			{
+				$path_owner = $parseval;
+			}
+		}
+	}
+	
+	return $path_owner;
+}
+
 sub sudo_exec
 {
 	return "Bad Encryption Key" unless(decrypt_param(pop(@_)) eq "Encryption checking OK");
@@ -2866,22 +2993,24 @@ sub sudo_exec
 
 sub sudo_exec_without_decrypt
 {
-	my ($sudo_exec) = @_;
+	my ($sudo_exec, $as_user) = @_;
 	$sudo_exec =~ s/('+)/'"$1"'/g;
-	logger "Running the following command \"" . $sudo_exec . "\" with sudo.";
-	my $command = "echo '$SUDOPASSWD'|sudo -kS -p \"<prompt>\" su -c '$sudo_exec;echo \$?' root 2>&1";
+	if( !defined($as_user) )
+	{
+		$as_user = "root";
+	}
+	
+	my $command = "echo '$SUDOPASSWD'|sudo -kS -p \"\" su -c '$sudo_exec;echo \$?' $as_user 2>&1";
 	my @cmdret = qx($command);
-	$cmdret[0] =~ s/^<prompt>//g if defined $cmdret[0];
 	chomp(@cmdret);
 	my $ret = pop(@cmdret);
 	chomp($ret);
 	
 	if ("X$ret" eq "X0")
 	{
-		logger "Command \"" . $sudo_exec . "\" was successfully run with sudo.";
 		return "1;".encode_list(@cmdret);
 	}
-	logger "Command \"" . $sudo_exec . "\" run with sudo failed with exit code $ret.";
+	
 	return -1;
 }
 
@@ -2907,25 +3036,21 @@ sub secure_path_without_decrypt
 		}
 	}
 	
-	my $uid = `id -u`;
-	chomp $uid;
-	my $gid = `id -g`;
-	chomp $gid;
 	$file_path =~ s/('+)/'\"$1\"'/g;
 	if($action eq "chattr+i")
 	{
 		if(defined $returnType && $returnType eq "str"){
-			return 'chown -Rf '.$uid.':'.$gid.' \''.$file_path.'\' && chattr -Rf +i \''.$file_path.'\'';
+			return 'chattr -Rf +i \''.$file_path.'\'';
 		}else{
-			return sudo_exec_without_decrypt('chown -Rf '.$uid.':'.$gid.' \''.$file_path.'\' && chattr -Rf +i \''.$file_path.'\'');
+			return sudo_exec_without_decrypt('chattr -Rf +i \''.$file_path.'\'');
 		}
 	}
 	elsif($action eq "chattr-i")
 	{
 		if(defined $returnType && $returnType eq "str"){
-			return 'chattr -Rf -i \''.$file_path.'\' && chown -Rf '.$uid.':'.$gid.' \''.$file_path.'\'';
+			return 'chattr -Rf -i \''.$file_path.'\'';
 		}else{
-			return sudo_exec_without_decrypt('chattr -Rf -i \''.$file_path.'\' && chown -Rf '.$uid.':'.$gid.' \''.$file_path.'\'');
+			return sudo_exec_without_decrypt('chattr -Rf -i \''.$file_path.'\'');
 		}
 	}
 	
@@ -4183,7 +4308,9 @@ sub steam_workshop_without_decrypt
 		$download_method, $url_list, $filename_list) = @_;
 	
 	# Creates mods path if it doesn't exist
-	if ( check_b4_chdir($mods_full_path) != 0)
+	my $owner = SERVER_RUNNER_USER;
+	chomp $owner;
+	if ( check_b4_chdir($mods_full_path, $owner) != 0)
 	{
 		return -1;
 	}
@@ -4267,7 +4394,7 @@ sub steam_workshop_without_decrypt
 	
 	my $bash_scripts_path = MANUAL_TMP_DIR . "/home_id_" . $home_id;
 	
-	if ( check_b4_chdir($bash_scripts_path) != 0)
+	if ( check_b4_chdir($bash_scripts_path, $owner) != 0)
 	{
 		return -1;
 	}
